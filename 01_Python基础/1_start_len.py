b9d7bcf6b3c1407fb86beac74ed7feaacf77d3fa
FengJingTao <161535129@qq.com>
2026-09-17 13:36:05 +0800

    整理文件结构，准备双选会

---
+++ 01_Python基础/1_start_len.py
@@ -0,0 +1,19 @@
+#对字符求长度
+s = "Hello World"
+print(len( s ))
+
+#通过索引来获取单个字符
+print(s[len(s)-1])
+
+#布尔值
+b1 = True
+b2 = False
+
+#空值类型
+n = None
+
+#tape函数
+print(type(b1))
+print(type(s))
+print(type(n))
+print(type(1.5))
